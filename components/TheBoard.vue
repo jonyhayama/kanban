2fc93dfd8101c90bb39646a22d202646c92743a3
Jony Hayama <jonathan.hayama@gmail.com>
2023-01-06 16:53:52 -0300

    🚸 Add drag animation and handle

--- components/TheBoard.vue
+++ components/TheBoard.vue
@@ -54,12 +54,17 @@ const columns = ref<Column[]>([
     group="columns"
     item-key="id"
     class="flex gap-4 overflow-x-auto items-start"
+    :animation="150"
+    handle=".drag-handle"
   >
     <template #item="{ element: column }: { element: Column }">
       <div
         class="column bg-gray-200 p-5 rounded min-w-[250px]"
       >
-        <header class="font-bold mb-4">{{ column.title }}</header>
+        <header class="font-bold mb-4">
+          <DragHandle />
+          {{ column.title }}
+        </header>
         <TaskCard v-for="task in column.tasks" :key="task.id" :task="task" />
         <footer>
           <button
