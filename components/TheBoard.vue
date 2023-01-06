910f0996e2c5012f5fa9265b185aaf96438fca87
Jony Hayama <jonathan.hayama@gmail.com>
2023-01-06 16:12:50 -0300

    💄 Initial markup

---
+++ components/TheBoard.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import { nanoid } from "nanoid";
+import type { Column } from '~/types';
+
+const columns = ref<Column[]>([
+  {
+    id: nanoid(),
+    title: 'Backlog',
+    tasks: [
+      {
+        id: nanoid(),
+        title: 'Task 1',
+        createdAt: new Date(),
+      },
+      {
+        id: nanoid(),
+        title: 'Task 2',
+        createdAt: new Date(),
+      },
+      {
+        id: nanoid(),
+        title: 'Task 3',
+        createdAt: new Date(),
+      }
+    ]
+  },
+  {
+    id: nanoid(),
+    title: 'Selected for Dev',
+    tasks: []
+  },
+  {
+    id: nanoid(),
+    title: 'In Progress',
+    tasks: []
+  },
+  {
+    id: nanoid(),
+    title: 'QA',
+    tasks: []
+  },
+  {
+    id: nanoid(),
+    title: 'Done',
+    tasks: []
+  },
+]);
+</script>
+
+<template>
+  <div>
+    <div v-for="column in columns" :key="column.id">
+      <header>{{ column.title }}</header>
+      <p v-for="task in column.tasks" :key="task.id">{{ task.title }}</p>
+    </div>
+  </div>
+</template>
