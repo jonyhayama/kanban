70f7fcd1d867069d6719076b210101c3773c95a3
Jony Hayama <jonathan.hayama@gmail.com>
2023-01-24 14:28:49 -0300

    🚸 Allow add, edit and delete columns

--- components/TheBoard.vue
+++ components/TheBoard.vue
@@ -49,42 +49,68 @@ const columns = ref<Column[]>([
     tasks: []
   },
 ]);
+
+function createColumn() {
+  const column: Column = {
+    id: nanoid(),
+    title: '',
+    tasks: []
+  }
+  columns.value.push(column);
+  nextTick(() => {
+    (document.querySelector('.column:last-of-type .title-input') as HTMLInputElement).focus();
+  });
+}
 </script>
 
 <template>
-  <draggable
-    v-model="columns"
-    group="columns"
-    item-key="id"
-    class="flex gap-4 overflow-x-auto items-start"
-    :animation="150"
-    handle=".drag-handle"
-  >
-    <template #item="{ element: column }: { element: Column }">
-      <div
-        class="column bg-gray-200 p-5 rounded min-w-[250px]"
-      >
-        <header class="font-bold mb-4">
-          <DragHandle />
-          {{ column.title }}
-        </header>
-        <draggable
-          v-model="column.tasks"
-          :group="{ name: 'tasks', pull: ctrlKey ? 'clone' : true }"
-          item-key="id"
-          :animation="150"
-          handle=".drag-handle"
+  <div class="flex items-start overflow-x-auto gap-4">
+    <draggable
+      v-model="columns"
+      group="columns"
+      item-key="id"
+      class="flex gap-4 items-start"
+      :animation="150"
+      handle=".drag-handle"
+    >
+      <template #item="{ element: column }: { element: Column }">
+        <div
+          class="column bg-gray-200 p-5 rounded min-w-[250px]"
         >
-          <template #item="{ element: task }: { element: Task }">
-            <div>
-              <TaskCard :task="task" @delete="column.tasks = column.tasks.filter(t => t.id !== $event)" />
-            </div>
-          </template>
-        </draggable>
-        <footer>
-          <TaskNew @add="column.tasks.push($event)" />
-        </footer>
-      </div>
-    </template>
-  </draggable>
+          <header class="font-bold mb-4">
+            <DragHandle />
+            <input
+              class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+              @keyup.enter="($event.target as HTMLInputElement).blur()"
+              @keydown.backspace.delete="column.title === '' ? columns = columns.filter(c => c.id !== column.id) : null"
+              type="text"
+              v-model="column.title"
+            />
+          </header>
+          <draggable
+            v-model="column.tasks"
+            :group="{ name: 'tasks', pull: ctrlKey ? 'clone' : true }"
+            item-key="id"
+            :animation="150"
+            handle=".drag-handle"
+          >
+            <template #item="{ element: task }: { element: Task }">
+              <div>
+                <TaskCard :task="task" @delete="column.tasks = column.tasks.filter(t => t.id !== $event)" />
+              </div>
+            </template>
+          </draggable>
+          <footer>
+            <TaskNew @add="column.tasks.push($event)" />
+          </footer>
+        </div>
+      </template>
+    </draggable>
+    <button
+      @click="createColumn"
+      class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50"
+    >
+      + Add Another Column
+    </button>
+  </div>
 </template>
